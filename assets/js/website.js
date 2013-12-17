1e2da29df35ee8d1760ffccf23dde3778be1e540
Tianyu Shi <ts133@duke.edu>
2013-12-17 05:18:41 -0500

    website
    asdf

--- assets/js/website.js
+++ assets/js/website.js
@@ -9,6 +9,44 @@ $(document).ready(function(){
 	var $archiveSection = $("#archiveLoadSection");
 	var $postSection = $("#postLoadSection");
 
+	var disqus_shortname = 'tianyushi';
+	var disqus_identifier; //made of post id and guid
+	var disqus_url; //post permalink
+
+	function hi() {
+		console.log("hi");
+	}
+
+	// function loadDisqus(source, identifier, url) {
+	// 	// console.log("loadDisqus");
+
+	// 	if (window.DISQUS) {
+	// 		jQuery('#disqus_thread').insertAfter(source); //append the HTML after the link
+
+	// 	   //if Disqus exists, call it's reset method with new parameters
+	// 	   DISQUS.reset({
+	// 	   	reload: true,
+	// 	   	config: function () {
+	// 	   		this.page.identifier = identifier;
+	// 	   		this.page.url = url;
+	// 	   	}
+	// 	   });
+
+	// 	} else {
+
+	// 	   //insert a wrapper in HTML after the relevant "show comments" link
+	// 	   jQuery('<div id="disqus_thread"></div>').insertAfter(source);
+	// 	   disqus_identifier = identifier; //set the identifier argument
+	// 	   disqus_url = url; //set the permalink argument
+
+	// 	   //append the Disqus embed script to HTML
+	// 	   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+	// 	   dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+	// 	   jQuery('head').append(dsq);
+
+	// 	}
+	// }
+
 	window.onhashchange = function() {
 		page=window.location.hash.substring(1);
 		setPage(page);
@@ -79,8 +117,6 @@ $(document).ready(function(){
 	};
 
 	function setPage(page) {
-		page = page.substring(1);
-
 		if(page == "") {
 			page = "Home";
 		}
