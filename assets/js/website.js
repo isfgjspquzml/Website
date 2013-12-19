6ff1c2d5f3c86a61accf7792a44e5608bf41552d
Tianyu Shi <ts133@duke.edu>
2013-12-18 20:40:36 -0500

    starting portfolio section

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-	var activePage;
 	var click = $("#click")[0];
+	var activePage;
+
 	var $otherContent = $("#main-content");
 	var $mainPic = $("#main-pic");
 
+	// Blog Sections
 	var $blogSection = $("#main-blog");
 	var $mainLoadSection = $("#mainLoadSection");
 	var $archiveSection = $("#archiveLoadSection");
 	var $postSection = $("#postLoadSection");
 
-	var disqus_shortname = 'tianyushi';
-	var disqus_identifier; //made of post id and guid
-	var disqus_url; //post permalink
+	// Portfolio Sections
+	var $portfolioSection = $("#portfolioSection");
 
 	window.onhashchange = function() {
 		page=window.location.hash.substring(1);
@@ -41,16 +42,19 @@ $(document).ready(function(){
 		console.log("LoadContent (js)");
 		var href;
 		var blogPost;
+		var portSection;
 
 		if(page == "Home") {
 			console.log("Home");
 			$otherContent.fadeOut(200, function () {});
 			$blogSection.fadeOut(200, function () {});
+			$portfolioSection.fadeOut(200, function () {});
 			$mainPic.delay(200).fadeIn(300, function () {})
 		} else if (page.substring(0, 4) == "Blog") {
 			console.log("Blog");
 			$otherContent.fadeOut(200, function () {});
 			$mainPic.fadeOut(300, function () {});
+			$portfolioSection.fadeOut(200, function () {});
 			$blogSection.delay(200).fadeIn(200, function () {});
 			blogPost = page.substring(5);
 			if(blogPost == "") {
@@ -66,18 +70,30 @@ $(document).ready(function(){
 				$archiveSection.fadeOut(200, function () {});
 				$postSection.fadeOut(200, function() {
 					$postSection.hide().load(blogPost, function() {
-						$postSection.delay(200).fadeIn(200, function() {
-							$.getScript('../assets/js/blog.js');
-						})
+						$postSection.delay(200).fadeIn(200, function() {})
 					})
 				})
 			}
+		} else if(page.substring(0,9) == "Portfolio") {
+			$mainPic.fadeOut(300, function () {});
+			$blogSection.fadeOut(200, function () {});
+			$otherContent.fadeOut(200, function () {});
+			$portfolioSection.delay(200).fadeIn(200, function () {
+				portSection = page.substring(10);
+				console.log(portSection);
+				if(portSection == "") {
+
+				} else {
+				}
+			});
 		} else {
 			console.log("Other");
 			$mainPic.fadeOut(300, function () {});
 			$blogSection.fadeOut(200, function () {});
+			$portfolioSection.fadeOut(200, function () {});
 
-			href = "../assets/pages/" + page.toLowerCase() + ".html" + " #loadSection";
+			pageSplit = page.split('/')[0];
+			href = "../assets/pages/" + pageSplit.toLowerCase() + ".html" + " #loadSection";
 
 			$otherContent.find("#loadSection").fadeOut(200, function() {
 				$otherContent.hide().load(href, function() {
@@ -105,7 +121,10 @@ $(document).ready(function(){
 		if(activePage != null) {
 			$(activePage).removeClass("active");
 		}
-		activePage = document.getElementById(page);
+
+		pageSplit = page.split('/')[0];
+		activePage = document.getElementById(pageSplit);
+
 		$(activePage).addClass("active");
 		console.log("Loading " + page);
 
